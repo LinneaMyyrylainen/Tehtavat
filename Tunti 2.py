2497a52b4c9daa7bf372f5b5eab18b0b5a2fcd54
LinneaMyyrylainen <linneakm@metropolia.fi>
2026-02-11 10:37:48 +0200

    Viikko 2 tunti harjoitukset

--- Tunti 2.py
+++ Tunti 2.py
@@ -60,3 +60,10 @@ else:
 
 print("Jatketaan...")
 
+
+luku = int(input("Anna kokonaisluku: "))
+
+if luku <0:
+    print("Luvun itseisarvo on", luku*(-1))
+else:
+    print("Luvun itseisarvo on", luku)
