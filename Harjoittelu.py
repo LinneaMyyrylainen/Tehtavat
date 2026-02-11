91039149e80b8298772f1af3a6d7be12d7533b35
LinneaMyyrylainen <linneakm@metropolia.fi>
2026-02-11 09:24:02 +0200

    Viikko 2 tehtävät

---
+++ Harjoittelu.py
@@ -0,0 +1,34 @@
+nimi = input("Anna oma nimesi: ")
+
+print("Hello,", nimi,"!")
+print("Hello, " + nimi + "!")
+print(f"Hello, {nimi}!")
+
+nimi2 = "Linnea"
+print(f"Hello, {nimi} ja {nimi2}!")
+print("Hello, " + nimi + " ja " + nimi2 + "!")
+
+ika = 18
+pituus=167.5
+print(pituus)
+print(ika)
+
+numero1 = 100
+numero2 = "100"
+
+print(numero1 + numero1)
+print(numero2 + numero2)
+
+ika = float(input("Anna ikäsi "))
+lisa = 10
+print("Käyttäjän ikä on: ", ika + lisa)
+
+print(2 * 3)
+
+pituus = float(input("Kuinka pitkä olet? "))
+paino = float(input("Kuinka paljon painat? "))
+
+bmi = paino / (pituus / 100) **2
+print("BMI:si on", bmi)
+
+print("Tervehdys, " + Linnea)
