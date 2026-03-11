6aeb817144f0c5afc58a6c6bd309583dfc4b4d3c
LinneaMyyrylainen <linneakm@metropolia.fi>
2026-03-11 10:06:44 +0200

    kertaustunti tehtävät

---
+++ viikkoharjoitus5.py
@@ -0,0 +1,35 @@
+#lista
+
+nimet = ["Viivi", "Ahmed", "Pekka", "Olga", "Mary"]
+print(nimet)
+
+#lisää listan loppuun
+nimet.append("Matti")
+
+#poistaa listasta
+nimet.remove("Pekka")
+
+#lisää listaan tiettyyn paikkaan
+nimet.insert(4, "Matti")
+
+print(nimet)
+
+nimet2 = ["Allu", "Ninni"]
+
+#lisää listan 2 listan 1 perään
+nimet.extend(nimet2)
+print(nimet)
+print(nimet2)
+
+#kertoo missä kohtaa tietty nimi on listassa
+indeksi = nimet.index("Olga")
+
+#kertoo onko haluttu nimi listassa vai ei
+if "Matti" in nimet:
+    print("Matti löytyi!")
+else:
+    print("Ei mattia!")
+
+#järjestää nimet aakkosjärjestykseen
+nimet.sort()
+print(nimet)
